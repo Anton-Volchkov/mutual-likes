e29852b06ab2f1af727e1c7783556c7744b3bd3d
anton.volchkov.2001 <anton.volchkov.2001@gmail.com>
2020-03-30 10:32:40 +0300

    Add SignalR

--- src/app/fetch-data/fetch-data.component.ts
+++ src/app/fetch-data/fetch-data.component.ts
@@ -1,19 +1,64 @@
-import { Component, Inject } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { NgForm, NgModel} from '@angular/forms';
-
+import { HubConnection, HubConnectionBuilder,HubConnectionState,LogLevel} from '@microsoft/signalr';
+import * as signalR from '@microsoft/signalr';
 
 @Component({
   selector: 'app-fetch-data',
   templateUrl: './fetch-data.component.html'
 })
-export class FetchDataComponent {
+export class FetchDataComponent implements OnInit {
  
+  public _hubConnection: HubConnection;
   public user:User;
   public disabledForm:boolean = false;
   constructor(private http: HttpClient,  @Inject('BASE_URL') private baseUrl: string) {
    
   }
+
+  ngOnInit()
+  {
+    this.createConnection();
+    this.registerOnServerEvents();
+    this.startConnection();
+  }
+
+  private createConnection() {
+    this._hubConnection = new HubConnectionBuilder()
+      .withUrl( 'https://localhost:44358/server', {
+        skipNegotiation: true,
+        transport: signalR.HttpTransportType.WebSockets
+      })
+      .withAutomaticReconnect()
+      .configureLogging(LogLevel.Information)
+      .build();
+
+  }
+
+  private startConnection() {
+    if (this._hubConnection.state === HubConnectionState.Connected) {
+      return;
+    }
+
+    this._hubConnection.start().then(
+      () => {
+        console.log('Hub connection started!');
+      },
+      error => console.error(error)
+    );
+  }
+
+  private registerOnServerEvents(): void {
+   
+    this._hubConnection.on('sendToAll', (data: User) => {
+    
+     this.user = data;
+     this.disabledForm = false;
+    });
+
+  }
+
 
   Search(Form:NgForm)
  {
@@ -25,20 +70,20 @@ export class FetchDataComponent {
      return;
    }
    var data = Form.value;
+  
+   console.log(data.userId);
    if(!data.sex)
    {
      alert("Вы не указали фильтр!");
      return;
    }
+
+   this._hubConnection
+   .invoke('SendToCaller', data.userId,data.sex)
+   .catch(err => console.error(err));
+
    this.disabledForm = true;
-  this.http.get<User>("https://mutual-like-server.herokuapp.com/" + 'mutuallikes'+'?userid='+data.userId+'&sex='+data.sex).subscribe(result => {
-    this.user = result;
-    this.disabledForm = false;
-  }, error => {
-  
-    this.disabledForm = false;
-    alert(  "Что-то пошло не так! Произошла ошибка!");
-  });
+ 
  }
 }
 
